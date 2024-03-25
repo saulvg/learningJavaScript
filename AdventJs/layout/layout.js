354ba8adb1e06a6f3876c419a605d9374cf3b4a8
saulvg <saulvgproyecto@gmail.com>
2024-03-25 20:00:37 +0100

    start with page sketch

---
+++ AdventJs/layout/layout.js
@@ -0,0 +1,7 @@
+document.getElementById("main-header").innerHTML = `
+<header><h1>Esto es el header</h1></header>
+`;
+
+document.getElementById("main-footer").innerHTML = `
+<footer>Esto es el footer</footer>
+`;
